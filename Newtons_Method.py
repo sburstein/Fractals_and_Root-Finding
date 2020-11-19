ff714f8a17efd3959c0c8f324dd97fe3cb9df324
Scott Burstein <scott.burstein@duke.edu>
2020-11-18 19:00:11 -0500

    Newton's Method implemented

---
+++ Newtons_Method.py
@@ -0,0 +1,26 @@
+from scipy.misc import derivative
+#from sympy import diff
+
+def NewtonsMethod(f, initial, tolerance = 1e-10, max_iterations = 1000, debug = False):
+    
+    it = 0
+    x0 = initial
+
+    while True:
+        x1 = x0 - f(x0) / derivative(f, x0)
+        difference = abs(x1 - x0)
+        if (difference < tolerance) or it >= max_iterations:
+            break
+        it += 1
+        x0 = x1
+    return x0
+
+def f(x):
+    return x**2 + 1
+
+initial = -2
+approx_root = NewtonsMethod(f, initial)
+
+print('x: ', initial)
+print('approx root: ', approx_root)
+print("f(approx root) = ", (f(approx_root)))
